6e412d109cb84c7af7bc947eff274d221a23ce80
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-08-22 13:30:20 +0800

    更新代码

--- src/main/java/com/company/project/demoweb/common/JsonUtil.java
+++ src/main/java/com/company/project/demoweb/common/JsonUtil.java
@@ -11,20 +11,17 @@ public class JsonUtil {
     public static String toJson(Object obj) {
         ObjectMapper mapper = new ObjectMapper();
         String str = null;
-
         try {
             str = mapper.writeValueAsString(obj);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
         return str;
     }
 
     public static <T> T fromJson(String str) {
         ObjectMapper mapper = new ObjectMapper();
         T t = null;
-
         try {
             t = mapper.readValue(str, new TypeReference<T>() {
             });

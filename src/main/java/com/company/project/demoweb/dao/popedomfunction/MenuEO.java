afe9d57c82e48d25293b1b60720e2ab3b54ec066
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-08-22 17:28:28 +0800

    更新代码

---
+++ src/main/java/com/company/project/demoweb/dao/popedomfunction/MenuEO.java
@@ -0,0 +1,17 @@
+package com.company.project.demoweb.dao.popedomfunction;
+
+import java.util.List;
+
+/**
+ * Created by wangzhj on 2017/8/22.
+ */
+public class MenuEO {
+
+    private Long pfId;
+
+    private String pfName;
+
+    private String prPath;
+
+    private List<MenuEO> children;
+}

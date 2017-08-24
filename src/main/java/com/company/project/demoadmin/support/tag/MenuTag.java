8cc3ca013efd0d6663e170f3ee0e1a1fd20b3659
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-08-24 10:20:13 +0800

    更新代码

--- src/main/java/com/company/project/demoadmin/support/tag/MenuTag.java
+++ src/main/java/com/company/project/demoadmin/support/tag/MenuTag.java
@@ -1,7 +1,8 @@
 package com.company.project.demoadmin.support.tag;
 
-import com.company.project.demoadmin.dao.popedomfunction.MenuEO;
-import com.google.common.collect.Lists;
+import com.company.project.demoadmin.service.function.FunctionService;
+import com.company.project.demoadmin.service.function.MenuVO;
+import com.company.project.demoadmin.support.SpringContext;
 import com.google.common.collect.Maps;
 import org.beetl.core.Configuration;
 import org.beetl.core.GroupTemplate;
@@ -9,11 +10,14 @@ import org.beetl.core.Template;
 import org.beetl.core.resource.ClasspathResourceLoader;
 
 import javax.servlet.jsp.JspException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Created by wangzhj on 2017/8/22.
+ * 菜单Tag
+ *
+ * @author wangzhj
  */
 public class MenuTag extends BaseTag {
 
@@ -34,23 +38,13 @@ public class MenuTag extends BaseTag {
 
     @Override
     public int doEndTag() throws JspException {
-        List<MenuEO> menuEOLt = Lists.newArrayList();
-
-        MenuEO menuEO = new MenuEO();
-        menuEO.setPfName("权限管理");
-
-        menuEOLt.add(menuEO);
-
-        List<MenuEO> children = Lists.newArrayList();
-        MenuEO menuEO1 = new MenuEO();
-        menuEO1.setPfName("角色列表");
-        children.add(menuEO1);
-        menuEO.setChildren(children);
+        FunctionService functionService = SpringContext.getBean(FunctionService.class);
+        Collection<MenuVO> menuVOLt = functionService.getMenuLt(1L);
 
         Map data = Maps.newHashMap();
-        data.put("menuEOLt", menuEOLt);
-        template.binding(data);
+        data.put("menuVOLt", menuVOLt);
 
+        template.binding(data);
         write(template.render());
 
         return this.EVAL_PAGE;

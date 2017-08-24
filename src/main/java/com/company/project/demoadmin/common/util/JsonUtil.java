9cec15a17d31b0713cdcc1ad8e85b731048a654f
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-08-24 14:15:08 +0800

    更新代码

---
+++ src/main/java/com/company/project/demoadmin/common/util/JsonUtil.java
@@ -0,0 +1,33 @@
+package com.company.project.demoadmin.common.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ *
+ */
+public class JsonUtil {
+
+    public static String toJson(Object obj) {
+        ObjectMapper mapper = new ObjectMapper();
+        String str = null;
+        try {
+            str = mapper.writeValueAsString(obj);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return str;
+    }
+
+    public static <T> T fromJson(String str) {
+        ObjectMapper mapper = new ObjectMapper();
+        T t = null;
+        try {
+            t = mapper.readValue(str, new TypeReference<T>() {
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return t;
+    }
+}

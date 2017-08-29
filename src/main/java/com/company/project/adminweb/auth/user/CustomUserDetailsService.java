ca2f53a3f80c8f8e7d176bac3c03345ca1965183
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-08-29 14:23:04 +0800

    更新代码

--- src/main/java/com/company/project/adminweb/auth/user/CustomUserDetailsService.java
+++ src/main/java/com/company/project/adminweb/auth/user/CustomUserDetailsService.java
@@ -1,10 +1,15 @@
 package com.company.project.adminweb.auth.user;
 
+import com.company.project.adminweb.service.function.FunctionService;
+import com.company.project.adminweb.service.function.PermissionVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
 
 /**
  * 用户详情服务
@@ -15,6 +20,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomUserDetailsService.class);
 
+    @Autowired
+    private FunctionService functionService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -22,6 +29,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         //用户详情
         CustomUserDetails userDetails = new CustomUserDetails(username, "123123");
 
+        List<PermissionVO> permissionVOLt = functionService.getRolePermissionLt(1L);
         //返回
         return userDetails;
     }
